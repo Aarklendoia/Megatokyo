2c43248e939ee9d05566595bd501d9c737721c1b
Edouard Biton <edouard.biton@outlook.com>
2021-06-29 16:27:19 +0200

    Start project Megatokyo UWP from scrach.

---
+++ Megatokyo/Views/StripsDetailPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+using Megatokyo.Core.Models;
+using Megatokyo.Helpers;
+using Megatokyo.Services;
+using Megatokyo.ViewModels;
+
+using Microsoft.Toolkit.Uwp.UI.Animations;
+
+using Windows.System;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Navigation;
+
+namespace Megatokyo.Views
+{
+    public sealed partial class StripsDetailPage : Page
+    {
+        public StripsDetailViewModel ViewModel { get; } = new StripsDetailViewModel();
+
+        public StripsDetailPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            await ViewModel.LoadDataAsync();
+            ViewModel.Initialize(e.Parameter as string, e.NavigationMode);
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            base.OnNavigatingFrom(e);
+            if (e.NavigationMode == NavigationMode.Back)
+            {
+                NavigationService.Frame.SetListDataItemForNextConnectedAnimation(ViewModel.SelectedImage);
+                ImagesNavigationHelper.RemoveImageId(StripsViewModel.StripsSelectedIdKey);
+            }
+        }
+
+        private void OnPageKeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == VirtualKey.Escape && NavigationService.CanGoBack)
+            {
+                NavigationService.GoBack();
+                e.Handled = true;
+            }
+        }
+    }
+}
